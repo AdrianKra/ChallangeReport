733490761f91d60e90fc5611da024fb775b91049
Adrian Krafft <ak426083@fh-muenster.de>
2024-05-27 10:46:16 +0200

    added the remaining controller tests

--- src/test/java/blossom/reports_service/model/UpdateControllerTest.java
+++ src/test/java/blossom/reports_service/model/UpdateControllerTest.java
@@ -22,9 +22,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.Collections;
 import java.util.Date;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import org.mockito.BDDMockito;
 
 @WebMvcTest(UpdateController.class)
 public class UpdateControllerTest {
@@ -48,21 +47,22 @@ public class UpdateControllerTest {
   private Challenge challenge;
   private ChallengeReport challengeReport;
   private ChallengeSummary challengeSummary;
+  private ReportDTO dto;
 
   java.util.Date date = new Date();
 
   @BeforeEach
   public void setUp() {
     user = new User();
+    challenge = new Challenge("Challenge 1", "Description 1", Unit.KM, 2.0, date,
+        1, 2, user, Visibility.PUBLIC);
+    challengeReport = new ChallengeReport(challenge, user, date, "Description 1");
+    dto = new ReportDTO(1L, 1L, date, date, "Description 1", ChallengeStatus.OPEN);
   }
 
   // Test for createChallengeReport with status code 201
   @Test
   public void createChallengeReportTest() throws Exception {
-    challenge = new Challenge("Challenge 1", "Description 1", Unit.KM, 2.0, date,
-        1, 2, user, Visibility.PUBLIC);
-    challengeReport = new ChallengeReport(challenge, user, date, "Description 1");
-    var dto = new ReportDTO(1L, 1L, date, date, "Description 1", ChallengeStatus.OPEN);
 
     given(this.reportsService.createChallengeReport(any(ReportDTO.class)))
         .willReturn(challengeReport);
@@ -81,5 +81,167 @@ public class UpdateControllerTest {
         .andExpect(jsonPath("$.challenge.user.id").isEmpty())
         .andExpect(jsonPath("$.description").value("Description 1"))
         .andExpect(jsonPath("$.status").value("OPEN"));
+  }
+
+  // Test for createChallengeReport with status code 404 because of
+  // NotFoundException of challenge
+  @Test
+  public void createChallengeReportBadRequestTest() throws Exception {
+    given(this.reportsService.createChallengeReport(any(ReportDTO.class)))
+        .willThrow(new NotFoundException("Challenge not found"));
+
+    ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
+    String requestJson = ow.writeValueAsString(dto);
+
+    this.mvc.perform(post("/call/createChallengeReport")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andDo(print())
+        .andExpect(status().isNotFound());
+  }
+
+  // Test for createChallengeReport with status code 404 because of
+  // NotFoundException of user
+  @Test
+  public void createChallengeReportBadRequestTest2() throws Exception {
+    given(this.reportsService.createChallengeReport(any(ReportDTO.class)))
+        .willThrow(new NotFoundException("User not found"));
+
+    ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
+    String requestJson = ow.writeValueAsString(dto);
+
+    this.mvc.perform(post("/call/createChallengeReport")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andDo(print())
+        .andExpect(status().isNotFound());
+  }
+
+  // Test for createChallengeReport with status code 409 because of
+  // AlreadyExistsException of challengeReport
+  @Test
+  public void createChallengeReportBadRequestTest3() throws Exception {
+    given(this.reportsService.createChallengeReport(any(ReportDTO.class)))
+        .willThrow(new AlreadyExistsException("ChallengeReport already exists"));
+
+    ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
+    String requestJson = ow.writeValueAsString(dto);
+
+    this.mvc.perform(post("/call/createChallengeReport")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andDo(print())
+        .andExpect(status().isConflict());
+  }
+
+  // Test for updateReport with status code 201
+  @Test
+  public void updateReportTest() throws Exception {
+    given(this.reportsService.updateChallengeReport(any(Long.class), any(ReportDTO.class)))
+        .willReturn(challengeReport);
+
+    ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
+    String requestJson = ow.writeValueAsString(dto);
+
+    this.mvc.perform(put("/call/updateChallengeReport/1")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.challenge.title").value("Challenge 1"))
+        .andExpect(jsonPath("$.challenge.description").value("Description 1"))
+        .andExpect(jsonPath("$.challenge.user.id").isEmpty())
+        .andExpect(jsonPath("$.description").value("Description 1"))
+        .andExpect(jsonPath("$.status").value("OPEN"));
+  }
+
+  // Test for updateReport with status code 404 because of NotFoundException of
+  // challengeReport
+  @Test
+  public void updateReportBadRequestTest() throws Exception {
+    given(this.reportsService.updateChallengeReport(any(Long.class), any(ReportDTO.class)))
+        .willThrow(new NotFoundException("ChallengeReport not found"));
+
+    ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
+    String requestJson = ow.writeValueAsString(dto);
+
+    this.mvc.perform(put("/call/updateChallengeReport/1")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andDo(print())
+        .andExpect(status().isNotFound());
+  }
+
+  // Test for updateReport with status code 404 because of NotFoundException of
+  // challenge
+  @Test
+  public void updateReportBadRequestTest2() throws Exception {
+    given(this.reportsService.updateChallengeReport(any(Long.class), any(ReportDTO.class)))
+        .willThrow(new NotFoundException("Challenge not found"));
+
+    ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
+    String requestJson = ow.writeValueAsString(dto);
+
+    this.mvc.perform(put("/call/updateChallengeReport/1")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andDo(print())
+        .andExpect(status().isNotFound());
+  }
+
+  // Test for updateReport with status code 404 because of NotFoundException of
+  // user
+  @Test
+  public void updateReportBadRequestTest3() throws Exception {
+    given(this.reportsService.updateChallengeReport(any(Long.class), any(ReportDTO.class)))
+        .willThrow(new NotFoundException("User not found"));
+
+    ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
+    String requestJson = ow.writeValueAsString(dto);
+
+    this.mvc.perform(put("/call/updateChallengeReport/1")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andDo(print())
+        .andExpect(status().isNotFound());
+  }
+
+  // Test for deleteReport with status code 200
+  @Test
+  public void deleteReportTest() throws Exception {
+    this.mvc.perform(delete("/call/deleteChallengeReport/1"))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+
+  // Test for deleteReport with status code 404 because of NotFoundException of
+  // user
+  @Test
+  public void deleteReportBadRequestTest() throws Exception {
+    BDDMockito.willThrow(new NotFoundException("User not found")).given(this.reportsService)
+        .deleteChallengeReport(any(Long.class));
+
+    this.mvc.perform(delete("/call/deleteChallengeReport/1"))
+        .andDo(print())
+        .andExpect(status().isNotFound());
+  }
+
+  // Test for deleteReport with status code 404 because of NotFoundException of
+  // challenge
+  @Test
+  public void deleteReportBadRequestTest2() throws Exception {
+    BDDMockito.willThrow(new NotFoundException("Challenge not found")).given(this.reportsService)
+        .deleteChallengeReport(any(Long.class));
+
+    this.mvc.perform(delete("/call/deleteChallengeReport/1"))
+        .andDo(print())
+        .andExpect(status().isNotFound());
   }
 }
